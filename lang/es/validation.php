cd2b3496b71dec7bd8268b4e867c0d0a656c38cd
Federico González <federico1818@gmail.com>
2022-12-28 01:09:18 -0300

    add locale config

---
+++ lang/es/validation.php
@@ -0,0 +1,13 @@
+<?php
+
+return [
+
+    'min' => [
+        'string' => ':attribute debe tener al menos :min caracteres.',
+    ],
+
+    'attributes' => [
+        'password' => 'La contraseña'
+    ],
+
+];
