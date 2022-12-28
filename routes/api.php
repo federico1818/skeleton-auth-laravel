a470f99ecee90ef0be0d91cf3bcb65a2d3818050
Federico González <federico1818@gmail.com>
2022-12-28 00:45:38 -0300

    add register route

--- routes/api.php
+++ routes/api.php
@@ -1,19 +1,11 @@
 <?php
 
-use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
-/*
-|--------------------------------------------------------------------------
-| API Routes
-|--------------------------------------------------------------------------
-|
-| Here is where you can register API routes for your application. These
-| routes are loaded by the RouteServiceProvider within a group which
-| is assigned the "api" middleware group. Enjoy building your API!
-|
-*/
+use App\Http\Controllers\RegisterUserController;
 
-Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
-    return $request->user();
-});
+Route::resource('register', RegisterUserController::class)->only([
+    'store'
+])->names([
+    'store' => 'register'
+]);
